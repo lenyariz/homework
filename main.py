08f195b0fff5372419149d0d10fa40067e57829e
Lenya <lenyarizov.rizov@mail.ru>
2025-03-05 01:39:38 +0300

    Урок 1

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("Введите имя: ")
+age = input("Введите возраст: ")
+color = input("Введите цвет: ")
+word = input("Введите существительное: ")
+verb = input("Введите глагол: ")
+
+print("Привет! Меня зовут", name)
+print("Мой возраст:", age)
+print(color, word, verb, "меня по голове!")
